02d958c3b73c08f902ca652826431afd86f18ee4
machalaworklio <machala@worklio.cz>
2021-10-27 18:00:48 +0200

    sort finished

--- src/App.vue
+++ src/App.vue
@@ -69,16 +69,17 @@ export default defineComponent({
       // .map(vnitřní funkce) - metoda co zavolá funkci pro každou položku v array
     );
 
-    const contentSorted = computed(
-      () => [...contentSearch.value].sort((a, b) => {
-      if (sortBy.value === "value"){
-        a.number - b.number
+    const contentSorted = computed(() => {
+      if (sortBy.value === 'value') {
+        return [...contentSearch.value].sort((a, b) => a.number - b.number);
       } else {
-        Date.parse(b.time) - Date.parse(a.time)
-      }});
-        // nelze kopírovat přímo ref ale hodnotu z něj
-        // copy - ...lists -> musí referovat hodnotu .value
-      });
+        return [...contentSearch.value].sort(
+          (a, b) => Date.parse(b.time) - Date.parse(a.time)
+        );
+      }
+    });
+    // nelze kopírovat přímo ref ale hodnotu z něj
+    // copy - ...lists -> musí referovat hodnotu .value
 
     // sort
     const contentSearch = computed(
